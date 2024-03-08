d76281add9ab0ec367845c306da46e6f845eabc8
Carsten Kremser <github@carstenkremser.de>
2024-03-08 10:41:00 +0100

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -5,20 +5,28 @@ public class Main {
         // [] -> Array    0,  1, 2
         int [] numbers = {5, 10, 4};
 
-        int sum = numbers[1] + numbers[0];
-        System.out.println(sum);
-        numbers[2] = 5;
+        //printNumbers(numbers);
 
-        int [] nums = new int[3];
+        int [] nums = new int[4];
         nums[0] = 5;
         nums[1] = 10;
         nums[2] = 4;
+        nums[3] = 20;
+
         printNumbers(nums);
     }
 
     public static void printNumbers (int[] numbers) {
+        //   Zähler;    Läuft solange x;    Mache nach jedem Durchlauf
         for (int i = 0; i < numbers.length; i++) {
             System.out.println(numbers[i]);
+        }
+
+        System.out.println("===========");
+
+        // Erstelle für jeden Eintrag in
+        for (int number : numbers) {
+            System.out.println(number);
         }
     }
 }
